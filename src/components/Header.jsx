a5d054bbee05b5290274d26cfb0a1dd137ac7953
Ryan Forgie <ryan.j.forgie@gmail.com>
2023-05-15 15:53:07 +0100

    uppercase text in header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -23,10 +23,35 @@ function CloseIcon(props) {
 
 function FloppyDiscIcon(props) {
   return (
-    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
-      <path d="M6.75 19.25H17.25C18.3546 19.25 19.25 18.3546 19.25 17.25V9.82843C19.25 9.29799 19.0393 8.78929 18.6642 8.41421L15.5858 5.33579C15.2107 4.96071 14.702 4.75 14.1716 4.75H6.75C5.64543 4.75 4.75 5.64543 4.75 6.75V17.25C4.75 18.3546 5.64543 19.25 6.75 19.25Z" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
-      <path d="M8.75 19V15.75C8.75 15.1977 9.19772 14.75 9.75 14.75H14.25C14.8023 14.75 15.25 15.1977 15.25 15.75V19" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
-      <path d="M8.75 5V8.25" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
+    <svg
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+    >
+      <path
+        d="M6.75 19.25H17.25C18.3546 19.25 19.25 18.3546 19.25 17.25V9.82843C19.25 9.29799 19.0393 8.78929 18.6642 8.41421L15.5858 5.33579C15.2107 4.96071 14.702 4.75 14.1716 4.75H6.75C5.64543 4.75 4.75 5.64543 4.75 6.75V17.25C4.75 18.3546 5.64543 19.25 6.75 19.25Z"
+        stroke="currentColor"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M8.75 19V15.75C8.75 15.1977 9.19772 14.75 9.75 14.75H14.25C14.8023 14.75 15.25 15.1977 15.25 15.75V19"
+        stroke="currentColor"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M8.75 5V8.25"
+        stroke="currentColor"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
     </svg>
   )
 }
@@ -44,12 +69,30 @@ function MobileNavItem({ href, children }) {
 function MobileNavigation(props) {
   return (
     <Popover {...props}>
-      <Popover.Button className="px-4 py-2 text-sm font-medium backdrop-blur bg-zinc-900 text-zinc-200">
-      <svg width="24" height="24" fill="none" viewBox="0 0 24 24">
-        <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5" d="M4.75 5.75H19.25"/>
-        <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5" d="M4.75 18.25H19.25"/>
-        <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5" d="M4.75 12H19.25"/>
-      </svg>
+      <Popover.Button className="bg-zinc-900 px-4 py-2 text-sm font-medium text-zinc-200 backdrop-blur">
+        <svg width="24" height="24" fill="none" viewBox="0 0 24 24">
+          <path
+            stroke="currentColor"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="1.5"
+            d="M4.75 5.75H19.25"
+          />
+          <path
+            stroke="currentColor"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="1.5"
+            d="M4.75 18.25H19.25"
+          />
+          <path
+            stroke="currentColor"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="1.5"
+            d="M4.75 12H19.25"
+          />
+        </svg>
       </Popover.Button>
       <Transition.Root>
         <Transition.Child
@@ -61,7 +104,7 @@ function MobileNavigation(props) {
           leaveFrom="opacity-100"
           leaveTo="opacity-0"
         >
-          <Popover.Overlay className="fixed inset-0 z-50 backdrop-blur-sm bg-black/80" />
+          <Popover.Overlay className="fixed inset-0 z-50 bg-black/80 backdrop-blur-sm" />
         </Transition.Child>
         <Transition.Child
           as={Fragment}
@@ -74,18 +117,16 @@ function MobileNavigation(props) {
         >
           <Popover.Panel
             focus
-            className="fixed inset-x-4 top-8 z-50 origin-top rounded-3xl p-8 ring-1 bg-zinc-900 ring-zinc-800 font-mono uppercase"
+            className="fixed inset-x-4 top-8 z-50 origin-top rounded-3xl bg-zinc-900 p-8 font-mono uppercase ring-1 ring-zinc-800"
           >
             <div className="flex flex-row-reverse items-center justify-between">
               <Popover.Button aria-label="Close menu" className="-m-1 p-1">
                 <CloseIcon className="h-6 w-6 text-zinc-400" />
               </Popover.Button>
-              <h2 className="text-sm font-medium text-zinc-400">
-                Navigation
-              </h2>
+              <h2 className="text-sm font-medium text-zinc-400">Navigation</h2>
             </div>
             <nav className="mt-6">
-              <ul className="-my-2 divide-y text-base divide-zinc-100/5 text-zinc-300">
+              <ul className="-my-2 divide-y divide-zinc-100/5 text-base text-zinc-300">
                 <MobileNavItem href="/about">About</MobileNavItem>
                 <MobileNavItem href="/posts">Posts</MobileNavItem>
                 <MobileNavItem href="/uses">Uses</MobileNavItem>
@@ -107,9 +148,7 @@ function NavItem({ href, children }) {
         href={href}
         className={clsx(
           'relative block px-3 py-2 transition',
-          isActive
-            ? 'text-orange-400'
-            : 'hover:text-orange-400'
+          isActive ? 'text-orange-400' : 'hover:text-orange-400'
         )}
       >
         {children}
@@ -121,7 +160,7 @@ function NavItem({ href, children }) {
 function DesktopNavigation(props) {
   return (
     <nav {...props}>
-      <ul className="flex text-md font-medium font-mono text-zinc-200">
+      <ul className="text-md flex font-mono font-medium uppercase text-zinc-200">
         <NavItem href="/about">About</NavItem>
         <NavItem href="/posts">Posts</NavItem>
         <NavItem href="/uses">Uses</NavItem>
@@ -144,9 +183,7 @@ export function Header() {
       >
         {isHomePage && (
           <>
-            <div
-              className="order-last mt-[calc(theme(spacing.16)-theme(spacing.3))]"
-            />
+            <div className="order-last mt-[calc(theme(spacing.16)-theme(spacing.3))]" />
             <Container
               className="top-0 order-last -mb-3 pt-3"
               style={{ position: 'var(--header-position)' }}
@@ -154,8 +191,7 @@ export function Header() {
               <div
                 className="top-[var(--avatar-top,theme(spacing.3))] w-full"
                 style={{ position: 'var(--header-inner-position)' }}
-              >
-              </div>
+              ></div>
             </Container>
           </>
         )}
@@ -168,9 +204,9 @@ export function Header() {
             style={{ position: 'var(--header-inner-position)' }}
           >
             <div className="flex justify-between gap-4">
-              <Link href="/" className="pointer-events-auto pr-3 py-2 flex">
+              <Link href="/" className="pointer-events-auto flex py-2 pr-3">
                 <FloppyDiscIcon className="mr-4 text-orange-400" />
-                <h2 className="text-md font-medium font-mono text-zinc-200 hover:text-orange-400">
+                <h2 className="text-md font-mono font-medium uppercase text-zinc-200 hover:text-orange-400">
                   Ryan Forgie
                 </h2>
               </Link>
